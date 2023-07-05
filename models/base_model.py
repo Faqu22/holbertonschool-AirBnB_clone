e781b4ffa79e635a796ec529b7a2a828e8c1c9af
Faqu22 <facundoalvarez415@gmail.com>
2023-07-05 13:47:30 -0300

    Task 4, changes in method __init__

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,26 @@ import datetime
 class BaseModel:
     """Class BaseModel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if not key == "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
+        """print"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updates the public instance attribute 'updated_at' \
+            with the current datetime"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = str(type(self).__name__)
         new_dict["created_at"] = self.created_at.isoformat()
