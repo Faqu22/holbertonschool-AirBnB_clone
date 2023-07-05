f8f19273e903f45545a61ed0a6b2ffa7b87f1dda
Faqu22 <facundoalvarez415@gmail.com>
2023-07-05 12:53:49 -0300

    Fix problems with cls in methods

--- models/base_model.py
+++ models/base_model.py
@@ -12,15 +12,15 @@ class BaseModel:
 
     updated_at = datetime.datetime.now()
 
-    def __str__(cls):
-        return f"[{cls.__class__.__name__}] ({cls.id}) {cls.__dict__}"
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
 
-    def to_dict(cls):
-        new_dict = cls.__dict__.copy()
-        new_dict["__class__"] = str(type(cls).__name__)
-        new_dict["created_at"] = cls.created_at.isoformat()
-        new_dict["updated_at"] = cls.updated_at.isoformat()
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = str(type(self).__name__)
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
