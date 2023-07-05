94c1476c9b19ca458a063772719f8894c359f197
Faqu22 <facundoalvarez415@gmail.com>
2023-07-05 13:11:37 -0300

    Fix init of instances id, created, updated

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,10 @@ import datetime
 class BaseModel:
     """Class BaseModel"""
 
-    id = str(uuid.uuid4())
-    created_at = datetime.datetime.now()
-
-    updated_at = datetime.datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
