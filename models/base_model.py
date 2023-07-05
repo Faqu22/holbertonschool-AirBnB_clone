48fb4b277179c99ca4bcfd09a44bc1efaee6195c
Faqu22 <facundoalvarez415@gmail.com>
2023-07-05 13:13:15 -0300

    Fix with module Datetime

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
