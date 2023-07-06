a47f56c7034275969c69e83cb143a16660d37a42
Faqu22 <facundoalvarez415@gmail.com>
2023-07-06 16:03:36 -0300

    Requirements mandatory

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+"""Tests to BaseModel"""
+
+
+class BaseTest(unittest.TestCase):
+    """ Unit Tests class for base tests """
+    pass
---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import unittest
+from models.engine.file_storage import FileStorage
+"""Tests to FileStorage"""
+
+
+class BaseTest(unittest.TestCase):
+    """ Unit Tests class for base tests """
+    pass
