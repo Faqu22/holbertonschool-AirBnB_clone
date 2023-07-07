839ced79566775a65b519f5ca5419ea5e8136c16
Faqu22 <facundoalvarez415@gmail.com>
2023-07-07 13:08:00 -0300

    Add path complete in imports

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Base model"""
 import uuid
 import datetime
-from __init__ import storage
+from models.__init__ import storage
 
 
 class BaseModel:
@@ -12,12 +12,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if not key == "__class__":
-                    setattr(self, key, value)
-        else:
-            storage.new(self)
+        #if kwargs:
+         #   for key, value in kwargs.items():
+          #      if not key == "__class__":
+          #          setattr(self, key, value)
+        #else:
+         #   storage.new(self)
 
     def __str__(self):
         """print"""
