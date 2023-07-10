e0bd2a9b34e8cfe642c709e63ba3bd62b8538a7f
Pablo <p5.tacoronte@gmail.com>
2023-07-09 17:11:46 -0700

    file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+""" test file model """
+
 import unittest
 import json
 from models.base_model import BaseModel
@@ -6,20 +9,27 @@ from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
+    """class test"""
+
     def setUp(self):
+        """test 1"""
         self.storage = FileStorage()
 
     def test_file_path_default(self):
+        """test 2"""
         self.assertEqual(self.storage._FileStorage__file_path, "file.json")
 
     def test_objects_default(self):
+        """test 3"""
         self.assertIsInstance(self.storage._FileStorage__objects, dict)
 
     def test_all_returns_dictionary(self):
+        """test 4"""
         all_objects = self.storage.all()
         self.assertIsInstance(all_objects, dict)
 
     def test_new_adds_object_to_objects(self):
+        """test 5"""
         obj = BaseModel()
         self.storage.new(obj)
         obj_key = f"{obj.__class__.__name__}.{obj.id}"
@@ -27,6 +37,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(self.storage._FileStorage__objects[obj_key], obj)
 
     def test_save_writes_to_file(self):
+        """test 6"""
         obj = BaseModel()
         self.storage.new(obj)
         self.storage.save()
@@ -39,6 +50,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(file_data[obj_key], obj.to_dict())
 
     def test_reload_loads_objects_from_file(self):
+        """test 7"""
         obj = BaseModel()
         self.storage.new(obj)
         self.storage.save()
