8764cfc1fcd09573df42bb8abcfc8625c64617bc
Faqu22 <facundoalvarez415@gmail.com>
2023-07-09 22:39:57 -0300

    Fix error of element when no exist any element

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,9 +39,9 @@ class FileStorage():
         """ reload func """
         try:
             with open(FileStorage.__file_path, "r", encoding="UTF8") as i_file:
-                readed = json.load(i_file) if not None else []
+                readed = json.load(i_file)
                 for key, value in readed.items():
-                    objcls = readed[key]['__class__']
+                    objcls = value['__class__']
                     if objcls in FileStorage.cls.keys():
                         FileStorage.__objects[key] = self.cls[objcls](**value)
         except FileNotFoundError:
